b8e8c90cc15160afd82de05ac2d820db0b46e6ac
GOODEE <GOODEE@DESKTOP-7I2OH19>
2024-10-31 17:23:46 +0900

    create LoginController, MainController, OnInterruptor, OffInterruptor

--- src/main/java/com/example/sakila/SakilaApplication.java
+++ src/main/java/com/example/sakila/SakilaApplication.java
@@ -1,13 +1,34 @@
 package com.example.sakila;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.example.sakila.controller.OnInterceptor;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 @SpringBootApplication
-public class SakilaApplication {
-
-	public static void main(String[] args) {
+public class SakilaApplication implements WebMvcConfigurer {
+	@Autowired private OnInterceptor oninterceptor; 
+	@Autowired private OffInterceptor offinterceptor;
+	
+	
+	
+	public static void main(String[] args) { 
 		SpringApplication.run(SakilaApplication.class, args);
 	}
-
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {	
+	
+		registry.addInterceptor(oninterceptor).addPathPatterns("/on/**");
+		registry.addInterceptor(offinterceptor).addPathPatterns("/off/**");
+		WebMvcConfigurer.super.addInterceptors(registry);
+	}
+	
+	
 }
+
